cea34a3f662fbb1970e413e7b591a30fc15e8632
Rico <mailvonrico@gmail.com>
2025-09-01 20:58:03 +0200

    sidemenu behavior

--- src/app/routes/home/home.ts
+++ src/app/routes/home/home.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class Home {
+  faCopy = faCopy;
 
+  copyInstallCommand() {
+    navigator.clipboard.writeText('npm i odt-templater');
+  }
 }
