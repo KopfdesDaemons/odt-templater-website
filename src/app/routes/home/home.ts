8652f1b6aa2a0e8071e0b22fc30717f41186ae05
Rico <mailvonrico@gmail.com>
2025-09-05 19:33:29 +0200

    aria label, h1 font size

--- src/app/routes/home/home.ts
+++ src/app/routes/home/home.ts
@@ -1,5 +1,6 @@
 import { NgClass } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,8 +9,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
   copyButtonClicked = signal(false);
+  private meta = inject(Meta);
+  private title = inject(Title);
+
+  ngOnInit(): void {
+    this.setMetaTags();
+  }
 
   copyInstallCommand() {
     navigator.clipboard.writeText('npm i odt-templater');
@@ -20,5 +27,15 @@ export class Home {
     setTimeout(() => {
       this.copyButtonClicked.set(false);
     }, 2000);
+  }
+
+  setMetaTags() {
+    this.title.setTitle('odt-templater');
+    const description =
+      'A simple templating engine for OpenDocument Text (.odt) files that can be used in Node.js and in the browser.';
+    this.meta.addTags([
+      { property: 'og:description', content: description },
+      { name: 'description', content: description },
+    ]);
   }
 }
